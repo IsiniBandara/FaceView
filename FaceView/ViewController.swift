8d2b409bc9f2c7a09befabd4261c39a39b42e6a9
IsiniBandara <67850663+IsiniBandara@users.noreply.github.com>
2023-03-04 04:39:20 +0530

    Initial Commit

---
+++ FaceView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FaceView
+//
+//  Created by Isini Bandara on 2023-03-04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
